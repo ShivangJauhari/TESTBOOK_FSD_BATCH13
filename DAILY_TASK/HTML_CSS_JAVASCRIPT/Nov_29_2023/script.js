0caadd7d31498d57dee7bb5a999fd1536cbe75f3
ShivangJauhari <jauharishivang@gmail.com>
2023-12-09 19:27:17 +0530

    HTML_CSS_JAVASCRIPT DAILY_TASK

---
+++ DAILY_TASK/HTML_CSS_JAVASCRIPT/Nov_29_2023/script.js
@@ -0,0 +1,53 @@
+// P1 REdeclaration of variable
+
+// var x = 'Hello'
+
+// document.writeln('Here x is ' + x);
+
+// document.write("<br>")
+
+// var x = 'Hello JS';
+
+// document.writeln('Here x is ' + x);
+
+// P2 Arrays
+// const fruits = ["Apple", "Banana", "Orange"];
+
+// document.getElementById("demo").innerHTML = fruits;
+
+// fruits[0] = "Apricot";
+
+// fruits.push("Mango");
+
+// document.getElementById("demo1").innerHTML = fruits;
+
+// P3 Redecalring Let
+let x = 'Hello';
+
+// document.writeln('Here x is ' + x);
+
+// document.write("<br>")
+
+// {
+//     let x = 'Hello JS';
+//     // Here x is Hello JS
+
+//     document.writeln('Here x is ' + x);
+//     document.write("<br>")
+//     }
+
+// document.writeln('Here x is ' + x);
+
+// document.getElementById("demo").innerHTML = x;
+
+// P4 Const
+
+const mobile = ["Iphone", "Redmi", "Oppo"];
+
+document.getElementById("demo").innerHTML = mobile;
+
+mobile[1] = "Oneplus";
+
+mobile.push("Motto");
+
+document.getElementById("demo1").innerHTML = mobile;
