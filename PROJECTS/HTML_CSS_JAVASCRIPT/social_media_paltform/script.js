8d7195972c74b7a3458913ea78a7f6208c262643
ShivangJauhari <jauharishivang@gmail.com>
2024-02-07 10:03:12 +0530

    social media platform project having issue with the like share comment button for every postdiv created dynamically linking it to increment counter

--- PROJECTS/HTML_CSS_JAVASCRIPT/social_media_paltform/script.js
+++ PROJECTS/HTML_CSS_JAVASCRIPT/social_media_paltform/script.js
@@ -45,9 +45,67 @@ writePostButton.addEventListener("click", function () {
         postButton.addEventListener("click", function () {
         // creating a div to display the post
         var postDiv = document.querySelector(".post-display");
-        var postContent = document.createElement("p");
+        var postContent = document.createElement("div");
+        postContent.classList.add("post-content");
         postContent.textContent = postTextArea.value;
         postDiv.appendChild(postContent);
+
+       ///creating a like button for the post
+        var likeButton = document.createElement("button");
+        likeButton.textContent = "Like";
+        likeButton.classList.add("like-button");
+        postDiv.appendChild(likeButton);
+
+        // creating a counter for the like button
+        var likeCounter = document.createElement("span");
+        likeCounter.textContent = "0";
+        // Like counter id
+        likeCounter.id = "like-counter";
+        likeButton.appendChild(likeCounter);
+
+        // Add event listener to the like button
+        likeButton.addEventListener("click", function () {
+            incrementCounter("like-counter");
+        });
+
+        // creating a comment button for the post
+        var commentButton = document.createElement("button");
+        commentButton.textContent = "Comment";
+        commentButton.classList.add("comment-button");
+        postDiv.appendChild(commentButton);
+
+        // creating a counter for the comment button
+        var commentCounter = document.createElement("span");
+        commentCounter.textContent = "0";
+        // Comment counter id
+        commentCounter.id = "comment-counter";
+        commentButton.appendChild(commentCounter);
+
+        // Add event listener to the comment button
+        commentButton.addEventListener("click", function () {
+            incrementCounter("comment-counter");
+            
+        });
+
+        // creating a share button for the post
+        var shareButton = document.createElement("button");
+        shareButton.textContent = "Share";
+        shareButton.classList.add("share-button");
+        postDiv.appendChild(shareButton);
+
+        // creating a counter for the share button
+        var shareCounter = document.createElement("span");
+        shareCounter.textContent = "0";
+        // Share counter id
+        shareCounter.id = "share-counter";
+        shareButton.appendChild(shareCounter);
+
+        // Add event listener to the share button
+        shareButton.addEventListener("click", function () {
+            incrementCounter("share-counter");
+           
+        });   
+
         postTextArea.value = "";
         post.style.display = 'none';
         postDiv.style.display = 'block';
@@ -59,10 +117,12 @@ writePostButton.addEventListener("click", function () {
 
 });
 
+// function for incremneting the counter
 function incrementCounter(counterId) {
     var counterElement = document.getElementById(counterId);
     var counterValue = parseInt(counterElement.textContent);
     counterValue++;
     counterElement.textContent = counterValue;
 }
+
 
