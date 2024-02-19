6b369d98e52cb0a3d73a6678e91485f289def2e4
ShivangJauhari <jauharishivang@gmail.com>
2024-02-19 13:09:17 +0530

    Revert "Linear search and Linear seaarch with flow data was lost so i have to recommit the"
    This reverts commit 32faa25c28562921892a8d5cfd01e1be824b5a85.

--- Data_structure/binary_search/binary_search_do_while.js
+++ Data_structure/binary_search/binary_search_do_while.js
@@ -1,20 +1,20 @@
 // Binary search do while loop
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-key = 4;
+n = 4;
 
-function binarySearch(arr, key) {
+function binarySearch(arr, n) {
     let start = 0;
     let end = arr.length - 1;
     let mid;
     do {
         mid = Math.floor((start + end) / 2);
         // To check if the mid is the element
-        if (arr[mid] === key) {
+        if (arr[mid] === n) {
             return ("Element found at index " + mid + " and value is " + arr[mid]);
         }
         // To shift the end to the left of the mid
-        else if (arr[mid] > key) {
+        else if (arr[mid] > n) {
             end = mid - 1;
         } 
         // To shift the start to the right of the mid
@@ -26,4 +26,4 @@ function binarySearch(arr, key) {
     return ("Element not found and value returned is " + -1);
 }
 
-console.log(binarySearch(arr, key)); // 9
+console.log(binarySearch(arr, n)); // 9
