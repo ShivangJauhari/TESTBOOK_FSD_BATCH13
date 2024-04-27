07846f8411dc3dff01413d1473ab35602c2a3d0a
Shivang Jauhari <jauharishivang@gmail.com>
2024-04-27 14:03:52 +0530

    dailylogger some functianlity is added

--- PROJECTS/HTML_CSS_JAVASCRIPT/dailylogger/script.js
+++ PROJECTS/HTML_CSS_JAVASCRIPT/dailylogger/script.js
@@ -1,121 +1,103 @@
-// adding a variable for the current date and time
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+// console log the message "Hello World" to the console
+console.log("Hello World");
 
+// function to display the current date and time    
+function displayDateTime() {
+    // get the current date and time
+    let currentDateTime = new Date();
+    // select an HTML element
+    let dateTimeElement = document.getElementById('dateTime');
+    // display the current date and time in the selected HTML element
+    dateTimeElement.textContent = currentDateTime;
+}
 
-// create a log object having properties of date, time, log with log number and having behavior of creating a new log, updating a log and deleting a log
+// call the function to display the current date and time
 
-var log = {
-    date: dateTime,
-    log: [],
-    logNumber: 0,
-    createLog: function(logText){
-        this.logNumber++;
-        this.log.push({id: this.logNumber, logText: logText});
+setInterval(displayDateTime, 1000);
+
+//create the object to store the data of the user from userin.html form
+let user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: ''
+};
+
+// function to get the data from the user form
+function getUserData() {
+    // get the data from the user form
+    user.firstName = document.getElementById('firstName').value;
+    user.lastName = document.getElementById('lastName').value;
+    user.email = document.getElementById('email').value;
+    user.username = document.getElementById('username').value;
+    user.password = document.getElementById('password').value;
+    // display the data in the console
+    console.log(user);
+}
+
+// function to display the data from the user form
+function displayUserData() {
+    // select an HTML element
+    let userDataElement = document.getElementById('userData');
+    // display the data in the selected HTML element
+    userDataElement.textContent = JSON.stringify(user);
+}
+
+// create a object of log having properties of current date and time, edit, delete, create, display behaviors and list of logs with id
+
+let log = {
+    currentDateTime: '',
+    logs: [],
+    edit: function(id, newMessage) {
+        this.logs[id] = newMessage;
     },
-    updateLog: function(logId, logText){
-        for(var i = 0; i < this.log.length; i++){
-            if(this.log[i].id === logId){
-                this.log[i].logText = logText;
-            }
-        }
+    delete: function(id) {
+        this.logs.splice(id, 1);
     },
-    deleteLog: function(logId){
-        for(var i = 0; i < this.log.length; i++){
-            if(this.log[i].id === logId){
-                this.log.splice(i, 1);
-            }
-        }
+    create: function(message) {
+        this.currentDateTime = new Date();
+        this.logs.push({id: this.logs.length, message: message, dateTime: this.currentDateTime});
+    },
+    display: function() {
+        console.log(this.logs);
     }
+};
+
+// add event listener to the the new log button
+document.getElementById('newLog').addEventListener('click', getLogData);
+
+
+
+
+// function to get the data from the log form
+function getLogData() {
+    
+    // get the data from the log form
+    let logMessage = document.getElementById('logMessage').value;
+    // create a new log
+    log.create(logMessage);
+    // display the data in the console
+    console.log(log);
 }
 
 
-// create  a textarea element field to enter the log text
-var logText = document.createElement("textarea");
-logText.setAttribute("id", "logText");
-logText.setAttribute("rows", "4");
-logText.setAttribute("cols", "50");
-logText.setAttribute("placeholder", "Enter your log here");
-document.body.appendChild(logText);
-
-// create a button to create a log
-var createLogButton = document.createElement("button");
-createLogButton.innerHTML = "Create Log";
-document.body.appendChild(createLogButton);
-
-// create a button to update a log
-var updateLogButton = document.createElement("button");
-updateLogButton.innerHTML = "Update Log";
-document.body.appendChild(updateLogButton);
-
-// create a button to delete a log
-var deleteLogButton = document.createElement("button");
-deleteLogButton.innerHTML = "Delete Log";
-document.body.appendChild(deleteLogButton);
-
-// create a button to display the log
-var displayLogButton = document.createElement("button");
-displayLogButton.innerHTML = "Display Log";
-document.body.appendChild(displayLogButton);
-
-
-// add event listener to the create log button and link it to the create log function of the log object
-
-createLogButton.addEventListener("click", function(){
-    var logTextValue = document.getElementById("logText").value;
-    log.createLog(logTextValue);
-    console.log(log);
-});
-
-// add event listener to the update log button and link it to the create log function of the log object
-
-updateLogButton.addEventListener("click", function(){
-    var logTextValue = document.getElementById("logText").value;
-    var logId = prompt("Enter the log id to update");
-    log.updateLog(parseInt(logId), logTextValue);
-    console.log(log);
-});
-
-// add event listener to the delete log button and link it to the create log function of the log object
-
-deleteLogButton.addEventListener("click", function(){
-    var logId = prompt("Enter the log id to delete");
-    log.deleteLog(parseInt(logId));
-    console.log(log);
-});
-
-// add event listener to the display log button and add log objects keys and values to the html by creating a div element and appending it to the body
-
-var logDisplay = document.createElement("div");
-logDisplay.setAttribute("id", "logDisplay");
-document.body.appendChild(logDisplay);
-
-var logDisplayText = document.createElement("p");
-logDisplayText.innerHTML = "Date: " + log.date;
-logDisplay.appendChild(logDisplayText);
-
-var logDisplayText = document.createElement("p");
-logDisplayText.innerHTML = "Logs: ";
-logDisplay.appendChild(logDisplayText);
-
-for(var i = 0; i < log.log.length; i++){
-    var logDisplayText = document.createElement("p");
-    logDisplayText.innerHTML = "Log " + log.log[i].id + ": " + log.log[i].logText;
-    logDisplay.appendChild(logDisplayText);
+// display the log key and values in the .log-container div
+function displayLogs() {
+    // select an HTML element
+    let logElement = document.getElementById('log-container');
+    // display the data in the selected HTML element
+    logElement.textContent = JSON.stringify(log.logs);
 }
 
-
-
-
-displayLogButton.addEventListener("click", function(){
-    console.log(log);
-});
+// call the function to display the logs
+displayLogs();
 
 
 
 
 
-//log the log object in the console
-console.log(log);
+
+
+
+
