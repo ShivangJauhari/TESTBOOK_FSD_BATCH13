3f6f3e3961a5b0de90c5aff12233b1cab081cdc3
Shivang Jauhari <jauharishivang@gmail.com>
2024-04-29 12:02:18 +0530

    node_learning

---
+++ DAILY_TASK/BACKEND/second.js
@@ -0,0 +1,19 @@
+const studentDetails = {
+    name: "John",
+    courses: ["Math", "Science", "English"],
+    marks: [80, 90, 85]
+}
+
+function smartwork(){
+    console.log("Smartwork");
+}
+
+function hardwork(){
+    console.log("Hardwork");
+}
+
+
+module.exports = {studentDetails, smartwork, hardwork};
+
+// hardwork();
+// smartwork();
